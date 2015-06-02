b7732269694af164156d28d2f2bcc84071f13100
Luiz Branco <me@luizbranco.com>
2015-06-02 02:41:50 -0400

    Add fn replace color mask

---
+++ identico.go
@@ -0,0 +1,26 @@
+package identico
+
+import (
+	"image"
+	"image/color"
+)
+
+func ReplaceMask(src image.Image, col color.NRGBA) image.Image {
+	bounds := src.Bounds()
+	dst := image.NewRGBA(bounds)
+	w, h := bounds.Max.X, bounds.Max.Y
+
+	for x := 0; x < w; x++ {
+		for y := 0; y < h; y++ {
+			rgba := col
+			pixel := src.At(x, y).(color.NRGBA)
+			if pixel.A != 0 {
+				rgba.A = pixel.A
+				dst.Set(x, y, rgba)
+			} else {
+				dst.Set(x, y, pixel)
+			}
+		}
+	}
+	return dst
+}
