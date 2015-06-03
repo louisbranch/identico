28bc88193181ca28976c9fa3ce32818c11037d94
Luiz Branco <me@luizbranco.com>
2015-06-03 01:39:56 -0400

    Pass color interface to fn

--- identico.go
+++ identico.go
@@ -5,14 +5,14 @@ import (
 	"image/color"
 )
 
-func ReplaceMask(src image.Image, col color.NRGBA) image.Image {
+func ReplaceMask(src image.Image, col color.Color) image.Image {
 	bounds := src.Bounds()
 	dst := image.NewRGBA(bounds)
 	w, h := bounds.Max.X, bounds.Max.Y
 
 	for x := 0; x < w; x++ {
 		for y := 0; y < h; y++ {
-			rgba := col
+			rgba := col.(color.NRGBA)
 			pixel := src.At(x, y).(color.NRGBA)
 			if pixel.A != 0 {
 				rgba.A = pixel.A
